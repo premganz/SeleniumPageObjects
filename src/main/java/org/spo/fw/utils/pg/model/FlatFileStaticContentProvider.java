9993bcf1cb923954cce1b4e29f77d4a60ff686ff
prem <premrocks@rediffmail.com>
2017-07-03 17:41:27 +0530

    pg-fix

--- src/main/java/org/spo/fw/utils/pg/model/FlatFileStaticContentProvider.java
+++ src/main/java/org/spo/fw/utils/pg/model/FlatFileStaticContentProvider.java
@@ -1,9 +1,18 @@
 package org.spo.fw.utils.pg.model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.spo.fw.config.SessionContext;
 import org.spo.fw.log.Logger1;
 import org.spo.fw.utils.pg.itf.StaticContentProvider;
 import org.spo.fw.web.ServiceHub;
@@ -12,8 +21,40 @@ public class FlatFileStaticContentProvider implements StaticContentProvider {
 	Logger1 log = new Logger1(this.getClass().getSimpleName());
 	@Override
 	public List<String> getContent(String expression, ServiceHub kw) {
-	//TODO to implement
-		return null;
+		String filePath =expression;
+		String result="";
+		try {
+			result=FileUtils.readFileToString(new File(filePath), "UTF-8");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		String folder = expression.substring(0,expression.lastIndexOf("/") );
+		String folderName = folder.substring(folder.lastIndexOf("/") , folder.length());
+		Properties p = new Properties();
+		File f = new File(folder+"/"+folderName+".properties");
+		if(f.exists()){
+			try {
+
+				p.load(new BufferedReader(new FileReader(f)));
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+				log.error("The file system.properties need to be present in your working directory defined by your strategy.textFilesPath");
+
+			}catch (IOException e1) {
+				//e1.printStackTrace();
+				log.error("The file system.properties there but  not read");
+
+			} 
+			for (String name : p.stringPropertyNames()) {
+				String value = p.getProperty(name);
+				System.setProperty(name, value);
+			}
+			result = substituteDynaVals(result, p);
+		}
+		ArrayList<String> resultList= new ArrayList<String>();
+		resultList.addAll(Arrays.asList(result.split("\n")));
+		return resultList;
+
 	}
 
 	
